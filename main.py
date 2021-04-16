b85e4dea3a184f2bb18e4eb48fd231b7013742ef
MaheshNat <maheshkumar.natamai@gmail.com>
2021-04-16 15:05:50 -0500

    added /user route to check if user exists/is private

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cloudscraper
 app = Flask(__name__)
 scraper = cloudscraper.create_scraper()
 
-@app.route('/<username>')
+@app.route('/trades/<username>')
 def trades(username):
     json = scraper.get(f'https://www.etoro.com/api/streams/v2/streams/user-trades/{username}').json()
     return jsonify(json)
@@ -12,6 +12,17 @@ def trades(username):
 @app.route('/ping')
 def ping():
     return jsonify({'message': 'PONG!'})
+
+@app.route('/user/<username>')
+def user(username):
+    try:
+        data = scraper.get(f'https://www.etoro.com/api/logininfo/v1.1/users/{username}').json()
+        if data['optOut']: 
+            return jsonify({'private': True})
+        else:
+            return jsonify({'exists': True})
+    except:
+        return jsonify({'exists':False})
 
 # @app.route('/general')
 # def general():
