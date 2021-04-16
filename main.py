b0960b3c3da194c47c807fc3230716756994f1ff
MaheshNat <maheshkumar.natamai@gmail.com>
2021-04-16 10:41:54 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import cloudscraper
+
+app = Flask(__name__)
+scraper = cloudscraper.create_scraper()
+
+@app.route('/<username>')
+def trades(username):
+    json = scraper.get(f'https://www.etoro.com/api/streams/v2/streams/user-trades/{username}').json()
+    return jsonify(json)
+
+# @app.route('/general')
+# def general():
+#     json = scraper.get('https://www.etoro.com/api/streams/v2/streams/user-trades/theryall').json()
+#     return jsonify(json)
+
+if __name__ == '__main__':
+    app.run(debug=True)
